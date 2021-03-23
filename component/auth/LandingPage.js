9ec95de04ce58ef0ead784ad34e0963f7cc0a8e5
bgunturk <bulent.gunturk@gmail.com>
2021-03-23 18:06:41 +0300

    secondcommit

--- component/auth/LandingPage.js
+++ component/auth/LandingPage.js
@@ -1,51 +1,15 @@
-import React, { Component } from 'react'
-import { View, Button, TextInput } from 'react-native'
+import React from 'react'
+import { Text, View, Button } from 'react-native'
 
-import firebase from 'firebase'
-
-export class Login extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            email: '',
-            password: '',
-        }
-
-        this.onSignUp = this.onSignUp.bind(this)
-    }
-
-    onSignUp() {
-        const { email, password } = this.state;
-        firebase.auth().signInWithEmailAndPassword(email, password)
-            .then((result) => {
-                console.log(result)
-            })
-            .catch((error) => {
-                console.log(error)
-            })
-    }
-
-    render() {
-        return (
-            <View>
-                <TextInput
-                    placeholder="email"
-                    onChangeText={(email) => this.setState({ email })}
-                />
-                <TextInput
-                    placeholder="password"
-                    secureTextEntry={true}
-                    onChangeText={(password) => this.setState({ password })}
-                />
-
-                <Button
-                    onPress={() => this.onSignUp()}
-                    title="Sign In"
-                />
-            </View>
-        )
-    }
+export default function Landing({ navigation }) {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+            <Button
+                title="Register"
+                onPress={() => navigation.navigate("Register")} />
+            <Button
+                title="Login"
+                onPress={() => navigation.navigate("Login")} />
+        </View>
+    )
 }
-
-export default Login
